4351cd54e5995b63d689f15a1f5d7bf01fe03f63
Fred Bauer <fred@bauer.xyz>
2020-02-05 17:49:06 -0500

    edit action added

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,7 +2,7 @@ class TodosController < ApplicationController
   def new
     @todo=Todo.new
   end
-
+#new submits to Create
   def create
     @todo = Todo.new(todo_params) #strong parameters
     if @todo.save #try and save
@@ -17,8 +17,22 @@ class TodosController < ApplicationController
     @todo=Todo.find(params[:id])
   end
 
-  private
-def todo_params
+  def edit
+    @todo=Todo.find(params[:id]) #copied from show, since we need to find what we're editing first
+  end
+#edit submits to update
+def update
+  @todo = Todo.find(params[:id]) #strong parameters
+  if @todo.update(todo_params) #try and update
+    flash[:notice] = "Todo was updated sucesfully!"
+    redirect_to todo_path(@todo) #works on save
+  else
+    render 'edit'
+  end
+end
+
+private
+def todo_params #parameter validation
   params.require(:todo).permit(:name, :description)
 end
 
