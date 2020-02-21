dbd48c06e4c23b9a6b8c4b566179d3042f7fffa3
Bauer <bauerfr@IGEN.LOCAL>
2020-02-21 17:36:41 -0500

    refactored (obfuscated)

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,6 @@
 class TodosController < ApplicationController
+  before_action :set_todo, only: [:edit, :update, :show, :destroy]
+
   def new
     @todo=Todo.new
   end
@@ -14,15 +16,15 @@ class TodosController < ApplicationController
   end
 
   def show
-    @todo=Todo.find(params[:id])
+
   end
 
   def edit
-    @todo=Todo.find(params[:id]) #copied from show, since we need to find what we're editing first
+
   end
 #edit submits to update
 def update
-  @todo = Todo.find(params[:id]) #strong parameters
+
   if @todo.update(todo_params) #try and update
     flash[:notice] = "Todo was updated sucesfully!"
     redirect_to todo_path(@todo) #works on save
@@ -36,7 +38,6 @@ def index
 end
 
 def destroy
-  @todo=Todo.find(params[:id])
   @todo.destroy
   flash[:notice] = "Todo was deleted sucesfully"
   redirect_to todos_path
@@ -44,6 +45,11 @@ end
 
 
 private
+def set_todo
+    @todo=Todo.find(params[:id]) #strong parameters
+end
+
+
 def todo_params #parameter validation
   params.require(:todo).permit(:name, :description)
 end
